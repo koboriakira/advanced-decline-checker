2b9faf0aac66c1d3f82e322bfaf62b80446269d5
Akira Kobori <private.beats@gmail.com>
2020-10-06 17:48:08 +0900

    メソッドをPOSTに変更

--- advanced_decline_checker/main.py
+++ advanced_decline_checker/main.py
@@ -4,7 +4,7 @@ from advanced_decline_checker.controller import check_ratio
 app = FastAPI()
 
 
-@app.get('/')  # methodとendpointの指定
-async def hello():
+@app.post('/')  # methodとendpointの指定
+def hello():
     is_success = check_ratio()
     return {"statusCode": 200 if is_success else 500}
